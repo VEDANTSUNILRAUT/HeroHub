eec4b55de8c8155b5fa18a001cd1b54cf97421b4
VED <vedantsunilraut@gmail.com>
2026-06-01 23:26:59 +0530

    feat(phase-3): implement launcher icon and branded splash screen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,6 +47,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.compose.ui.tooling.preview)
+    implementation("com.google.android.material:material:1.12.0")
 
     debugImplementation(libs.androidx.compose.ui.tooling)
 
@@ -65,4 +66,7 @@ dependencies {
     // Koin
     implementation(libs.koin.android)
     implementation(libs.koin.compose)
+
+    //SplashScreen
+    implementation("androidx.core:core-splashscreen:1.0.1")
 }
--- app/src/main/java/com/vedantraut/herohub/MainActivity.kt
+++ app/src/main/java/com/vedantraut/herohub/MainActivity.kt
@@ -4,23 +4,22 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.vedantraut.herohub.presentation.home.HomeScreen
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.vedantraut.herohub.ui.theme.HeroHubTheme
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        installSplashScreen()
+
         super.onCreate(savedInstanceState)
+
         enableEdgeToEdge()
+
         setContent {
-            setContent {
-                HomeScreen()
+            HeroHubTheme {
+                HeroHubAppContent()
             }
         }
     }
